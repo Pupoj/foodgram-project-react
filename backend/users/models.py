5fe0bc1faa2d15cd617efde0b6a9fb5f542c88e2
Pupok <nasyr02001@gmail.com>
2023-10-11 15:22:04 +0500

    users done

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,71 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Класс для пользователей."""
+
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=150,
+        unique=True,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+    )
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        max_length=254,
+        unique=True,
+
+    )
+
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('username', 'email'),
+                name='unique_user'
+            )
+        ]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        verbose_name='Подписчик',
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='following',
+        verbose_name='Автор',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'Автор: {self.author}, подписчик: {self.user}'
+
+    def save(self, **kwargs):
+        if self.user == self.author:
+            raise ValidationError("Невозможно подписаться на себя")
+        super().save()
+
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'],
+                name='unique_follower')
+        ]
