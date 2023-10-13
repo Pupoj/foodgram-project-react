bb971f10757ae3b821f31e153412769fffd21ee7
Влад Насыров <nasyr02001@gmail.com>
2023-10-13 13:02:02 +0500

    t

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
-    'users',
-    'recipes',
-    'api',
+    'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
