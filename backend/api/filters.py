f7750a95241de75e421225841c16bc35b0c95814
Влад Насыров <nasyr02001@gmail.com>
2023-10-14 19:33:58 +0500

    test

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -22,7 +22,7 @@ class RecipeFilter(FilterSet):
     )
 
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
+    in_shopping_cart = filters.BooleanFilter(method='filter_in_shopping_cart')
 
     class Meta:
         model = Recipe
@@ -34,7 +34,7 @@ class RecipeFilter(FilterSet):
             return queryset.filter(favorites__user=user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset, name, value):
+    def filter_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value and not user.is_anonymous:
             return queryset.filter(shopping_cart__user=user)
