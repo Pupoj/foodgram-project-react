0128bb6b46905b05df92397cf37d649232656566
Влад Насыров <pupok-mac@MacBook-Air-Vlad.local>
2023-10-18 12:36:43 +0500

    done

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
-
 from recipes.models import Ingredient, Recipe, Tag
 
 User = get_user_model()
@@ -37,7 +36,7 @@ class RecipeFilter(FilterSet):
             return queryset.filter(favorites__user=user)
         return queryset
 
-    def filter_in_shopping_cart(self, queryset, name, value):
+    def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value and not user.is_anonymous:
             return queryset.filter(shopping_cart__user=user)
